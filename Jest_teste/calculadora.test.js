6d86a6ae1e7d6337e3f7746e7f462020fa801004
ALICE DOS PASSOS SILVA <a.passos@labcetec.net>
2024-10-07 10:51:13 -0300

    Modificação de informações

--- Jest_teste/calculadora.test.js
+++ Jest_teste/calculadora.test.js
@@ -9,22 +9,22 @@ test('teste de funcionalidade soma', () => {
   })
 
   test('teste de funcionalidade soma', () => {
-    expect(calculadora("-" , [1, 2, 3])).toBe(6);
+    expect(calculadora("-" , [1, 2, 3])).not.toBe(4);
   })
 
   test('teste de funcionalidade soma', () => {
-    expect(calculadora("/" , [1, 2, 3])).toBe(6);
+    expect(calculadora("/" , [1, 2, 3])).not.toBe(6);
   })
 
   test('teste de funcionalidade soma', () => {
-    expect(calculadora("*" , [1, 2, 3])).toBe(6);
+    expect(calculadora("*" , [1, 2, 3])).not.toBe(6);
   })
 
   test('teste de funcionalidade soma', () => {
-    expect(calculadora("+" , [56, 6.8, 90])).toBe(6);
+    expect(calculadora("+" , [56, 6.8, 90])).toBe(152.8);
   })
   
   test('teste de funcionalidade soma', () => {
-    expect(calculadora("+" , [2.2, 2.2, 30])).toBe(6);
+    expect(calculadora("+" , [2.2, 2.2, 30])).not.toBe(6);
   })
 
